1a3412dd27863080fd307222d7d990d79c484ab2
quratulainasim <quratulainasim1234@gmail.com>
2025-05-10 22:53:47 +0500

    Update main.py

--- 25-projects/rock-papper-scissor/main.py
+++ 25-projects/rock-papper-scissor/main.py
@@ -12,11 +12,11 @@ while True:
     if user_input == computer_input:
         print("Game is tie!")
     elif user_input == "rock" and computer_input == "paper":
-        print("You Win")
+        print("Computer Win")
     elif user_input == "paper" and computer_input == "scissors":
-        print("You Win")
+        print("Computer Win")
     elif user_input == "scissors" and computer_input == "rock":
-        print("You Win")
+        print("Computer Win")
      
     else:
-        print("Computer Win!")
+        print("You Win!")
